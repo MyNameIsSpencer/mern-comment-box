e91fb915febd94a89d66fb65b3c12faa55f25968
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-01 19:28:30 -0400

    add model, paste commentbox/list/form.js and style

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CommentBox />, document.getElementById('root'));
