0cbe1cf00ff3aed6c5d0a1c9726c8708afc13090
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-02 02:16:54 -0400

    add to comments.js and server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Comment = require('./model/comments');
 
 // create instances
 var app = express();
@@ -9,6 +10,9 @@ var router = express.Router();
 
 //set port
 var port = process.env.API_PORT || 3001;
+
+// db config
+mongoose.connect(' mongodb://myMernCommentBoxUser:myMernCommentBoxPassword555@ds019678.mlab.com:19678/mymerncommentbox');
 
 
 // now we should configure the API to use bodyParser
@@ -28,3 +32,12 @@ app.use(function(req, res, next) {
 // and remove cacheing so we get most recent Comments
   res.setHeader('Cache-Control', 'no-cache');
   next();
+});
+
+//now we can set the route path & initialize API
+app.use('/api', router);
+
+//starts the server and listens requests
+app.listen(port, function() {
+  console.log(`api running on port ${port}`);
+});
