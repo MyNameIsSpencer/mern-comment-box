ffee62dd5bb4af00a73bddb7525aaedad5880dc6
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-01 23:03:21 -0400

    add content to comment commentForm commentList, data, style

---
+++ server.js
@@ -0,0 +1,30 @@
+// import dependencies
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+// create instances
+var app = express();
+var router = express.Router();
+
+//set port
+var port = process.env.API_PORT || 3001;
+
+
+// now we should configure the API to use bodyParser
+// and look for JSON data in request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Prevent errors from Cross Origin Resource Shraing,
+// set headers to allow CORS with middleware:
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods',
+  'GET, HEAD, OPTIONS, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Contorl-Request-Method, Access-Control-Request-Headers');
+
+// and remove cacheing so we get most recent Comments
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
